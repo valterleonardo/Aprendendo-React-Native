7f96029506db84e7c892a3ea75a82b83f1179c0d
Valter Bortoletto <valter.bortoletto@softplan.com.br>
2020-03-29 08:14:22 -0300

    utilizando return implicito acme script

--- exercicio1/src/componentes/Simples.js
+++ exercicio1/src/componentes/Simples.js
@@ -1,6 +1,4 @@
 import React from 'react';
 import {Text} from 'react-native';
 
-export default props => {
-  return <Text>Arrow {props.texto}</Text>;
-};
+export default props => <Text>return implict Arrow {props.texto}</Text>;
