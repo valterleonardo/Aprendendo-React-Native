e20c06dd54dfae062c33a8bfdaac84fe094d769d
Valter Bortoletto <valter.bortoletto@softplan.com.br>
2020-03-29 08:13:33 -0300

    mesma funcao component utilizando acme script

--- exercicio1/src/componentes/Simples.js
+++ exercicio1/src/componentes/Simples.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Text} from 'react-native';
 
-export default function(props) {
-  return <Text>{props.texto}</Text>;
-}
+export default props => {
+  return <Text>Arrow {props.texto}</Text>;
+};
