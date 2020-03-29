4c8bdfd201e50b2d470eb15dad0831d5eaa40be6
Valter Bortoletto <valter.bortoletto@softplan.com.br>
2020-03-29 08:28:25 -0300

    criando style e utilizando no componente criando anteriormente

--- exercicio1/src/componentes/Simples.js
+++ exercicio1/src/componentes/Simples.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import {Text} from 'react-native';
+import Padrao from '../estilo/Padrao';
 
-export default props => <Text>return implict Arrow {props.texto}</Text>;
+export default props => (
+  <Text style={[Padrao.ex]}>return implict Arrow {props.texto}</Text>
+);
