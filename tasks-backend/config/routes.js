c8c41da5a54a85aba57ec5cf8aa261a2966622ef
Valter Bortoletto <valter.bortoletto@softplan.com.br>
2020-04-01 21:10:05 -0300

    finalizando

--- tasks-backend/config/routes.js
+++ tasks-backend/config/routes.js
@@ -1,4 +1,13 @@
 module.exports = app => {
   app.post('/signup', app.api.user.save)
   app.post('/signin', app.api.auth.signin)
+  app.route('/tasks')
+    .all(app.config.passport.authenticate())
+    .get(app.api.task.getTasks)
+    .post(app.api.task.save)
+
+  app.route('/tasks/:id')
+    .all(app.config.passport.authenticate())
+    .delete(app.api.task.remove)
+    .put(app.api.task.toggleTask)
 }
